ea6bca8e09a862a4792282c24c7afa3dd178a312
Tristan <bouschet.tristan@gmail.com>
2014-10-01 09:41:30 +0200

    Ajout multiplication et division

--- Appli1.c
+++ Appli1.c
@@ -5,9 +5,9 @@ int main()
 {
     printf("Bonjour, voici mon fichier C\n");
     int x = 0;
-    while(x != 1 && x !=2)
+    while(x != 1 && x != 2 && x!= 3 && x != 4)
     {
-        printf("Voulez-vous fair eune somme(1) ou une soustraction(2)?");
+        printf("Voulez-vous fair eune somme(1), une soustraction(2), une multiplication(3), une division(4)?");
         scanf("%d",&x);
     }
     if ( x == 1)
@@ -21,7 +21,7 @@ int main()
         int c = a + b;
         printf("La somme est: %d",c);
     }
-    else
+    else if (x == 2)
     {
         printf("Nous allons faire une soustraction\n");
         printf("Premier nombre: ");
@@ -32,6 +32,29 @@ int main()
         int c = a - b;
         printf("La différence est: %d",c);
     }
+	else if ( x == 3)
+    {
+        printf("Nous allons faire une multiplication\n");
+        printf("Premier nombre: ");
+        int a,b;
+        scanf("%d",&a);
+        printf("Deuxième nombre: ");
+        scanf("%d",&b);
+        int c = a * b;
+        printf("La multiplication est: %d",c);
+    }
+    else if (x == 4)
+    {
+        printf("Nous allons faire une division\n");
+        printf("Premier nombre: ");
+        int a,b;
+        scanf("%d",&a);
+        printf("Deuxième nombre: ");
+        scanf("%d",&b);
+        int c = a / b;
+        printf("La division est: %d",c);
+    }
+
 
 
     return 0;
