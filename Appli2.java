e4bdb637e42f6c1e47ae762fd0f452d192ceefd0
Tristan <bouschet.tristan@gmail.com>
2014-10-01 09:29:22 +0200

    Creation d'une classe voiture en java

---
+++ Appli2.java
@@ -0,0 +1,13 @@
+public class Voiture {
+
+    public Voiture() {
+    }
+
+    public Voiture(String couleur, int nbRoues) {
+        this.couleur = couleur;
+        this.nbRoues = nbRoues;
+    }
+    
+    String couleur;
+    int nbRoues;  
+}
