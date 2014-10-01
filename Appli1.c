e28d082e580b9aeaac1f85f28ed1eff8bdd5fcb1
Tristan <bouschet.tristan@gmail.com>
2014-10-01 09:22:23 +0200

    Ajout de la soustraction

--- Appli1.c
+++ Appli1.c
@@ -4,14 +4,34 @@
 int main()
 {
     printf("Bonjour, voici mon fichier C\n");
-    printf("Nous allons faire une somme\n");
-    printf("Premier nombre: ");
-    int a,b;
-    scanf("%d",&a);
-    printf("Deuxième nombre: ");
-    scanf("%d",&b);
-    int c = a + b;
-    printf("La somme est: %d",c);
+    int x = 0;
+    while(x != 1 && x !=2)
+    {
+        printf("Voulez-vous fair eune somme(1) ou une soustraction(2)?");
+        scanf("%d",&x);
+    }
+    if ( x == 1)
+    {
+        printf("Nous allons faire une somme\n");
+        printf("Premier nombre: ");
+        int a,b;
+        scanf("%d",&a);
+        printf("Deuxième nombre: ");
+        scanf("%d",&b);
+        int c = a + b;
+        printf("La somme est: %d",c);
+    }
+    else
+    {
+        printf("Nous allons faire une soustraction\n");
+        printf("Premier nombre: ");
+        int a,b;
+        scanf("%d",&a);
+        printf("Deuxième nombre: ");
+        scanf("%d",&b);
+        int c = a - b;
+        printf("La différence est: %d",c);
+    }
 
 
     return 0;
